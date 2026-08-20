a57798103ce6daaf2cb848d323ad5bbc447f9c04
lansogame <442972934@qq.com>
2026-08-20 20:10:10 +0800

    Change default log level from 'DEBUG' to 'Debug'

--- utils/config.py
+++ utils/config.py
@@ -53,7 +53,7 @@ def get_config():
         "browserTimeout": int(os.getenv("BROWSER_TIMEOUT", "120000")),  # 浏览器操作超时时间，单位毫秒
         "friendListTimeout": int(os.getenv("FRIEND_LIST_WAIT_TIME", "2000")),  # 好友列表加载超时时间，单位毫秒
         "taskRetryTimes": int(os.getenv("TASK_RETRY_TIMES", "3")),  # 任务重试次数
-        "logLevel": os.getenv("LOG_LEVEL", "DEBUG"),  # 日志级别
+        "logLevel": os.getenv("LOG_LEVEL", "Debug"),  # 日志级别
     }
 
     return config
